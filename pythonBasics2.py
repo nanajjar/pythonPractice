16da136e831961e0ebd2cc71964e6076ffe88781
Nadia Najjar <nanajjar@uncc.edu>
2020-10-08 12:08:28 -0400

    commit pythonBasics2 and test

---
+++ pythonBasics2.py
@@ -0,0 +1,23 @@
+# Python Activtiy
+#
+# Fill in the code for the functions below.
+# The starter code for each function includes a 'return'
+# which is just a placeholder for your code.
+
+# Part A. char_exists
+# Define a function char_exists(str,chr) that takes a string and a character
+# and returns the number of times (count) this character appeared in the string.
+# If the string is empty and the character is not in the string it should return zero
+def char_exists(s, char):
+    # YOUR CODE HERE
+
+    return
+
+
+# Part B. char_count
+# Define a function char_count(str,chr) that takes a string and a character
+# and returns the string after replacing all occurrences of this character with an underscore
+def char_remove(s, char):
+    # YOUR CODE HERE
+
+    return
